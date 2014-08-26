efa307133b566cc0c324fcdb8924b207aa289b25
Aaron Patterson <aaron.patterson@gmail.com>
2014-08-26 11:37:52 -0700

    adding tests for puma

--- lib/the_metal/puma.rb
+++ lib/the_metal/puma.rb
@@ -1,29 +1,45 @@
 require 'the_metal/request'
 require 'the_metal/response'
-require 'puma/cli'
+require 'puma'
 
 module TheMetal
   class Puma
     def initialize app
       @app = app
-      app.start_app
+      app.start_app if app.respond_to? :start_app
     end
 
     def call env
       env['rack.hijack'].call
       socket = env['rack.hijack_io']
 
+      default_headers = { }
       req = TheMetal::Request.new env
-      res = TheMetal::Response.new req, socket
+      res = TheMetal::Response.new 200, default_headers, socket
       @app.serve req, res
       [nil, {}, nil]
     end
+
+    class Proxy
+      def initialize app
+        @app = app
+        @serv = ::Puma::Server.new Puma.new @app
+      end
+
+      def stop
+        @serv.stop
+      end
+
+      def listen port, address
+        @serv.add_tcp_listener address, port
+        @serv.run
+        sleep
+      end
+    end
   end
 
-  def self.boot app
-    cli = ::Puma::CLI.new([])
-    cli.options[:app] = TheMetal::Puma.new(app)
-    cli.run
+  def self.create_server app
+    Puma::Proxy.new app
   end
 end
 
